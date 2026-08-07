2274fc25d16262b7dcd711e07c1551a297fd5038
Akash <akashgods222@gmail.com>
2026-08-07 17:32:49 +0530

    Add  Variables and data types practice

---
+++ 01-java-basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+  public static void main(String[] args) {
+    String name = "Akash";
+    int age = 21;
+    double cgpa = 8.5;
+    char grade = 'A';
+    boolean isStudent = true;
+   System.out.println("===============================");
+   System.out.println("      Student Information     ");
+   System.out.println("===============================");
+   System.out.println("Name:" + name);
+   System.out.println("Age:" + age);
+   System.out.println("CGPA:" + cgpa);
+   System.out.println("Grade:" + grade);
+   System.out.println("Is Student:" + isStudent);
+  }
+}
+      
