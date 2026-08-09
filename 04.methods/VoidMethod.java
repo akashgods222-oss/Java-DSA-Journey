e3afc7970b94544697af857d444e8e40c78398af
Akash <akashgods222@gmail.com>
2026-08-09 21:47:15 +0530

    Add VoidMethod class with printTable method

---
+++ 04.methods/VoidMethod.java
@@ -0,0 +1,13 @@
+public class VoidMethod {
+    static void printTable(int num) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num * i);
+           
+        }
+    }
+    public static void main(String[] args) {
+        printTable(50);
+       
+    }
+
+}
