62a25a3cbc2869f5c28e9789ac713bc5ac7d2c5d
Akash <akashgods222@gmail.com>
2026-08-23 21:53:32 +0530

    Add minimum element search in rotated  sorted array

---
+++ 06-arrays/binary-search/FindMin.java
@@ -0,0 +1,18 @@
+public class FindMin {
+    static int findMin(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid; 
+            }
+        }
+        return arr[left];
+    }
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[] {4,5,6,7,0,1,2}));
+    }
+}
