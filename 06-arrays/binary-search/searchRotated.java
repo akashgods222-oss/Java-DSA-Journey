0f2a07f97d686e6ff02028b4cb4b94f0708705d8
Akash <akashgods222@gmail.com>
2026-08-23 21:51:30 +0530

    Add search in rotated sorted array using binary search

---
+++ 06-arrays/binary-search/searchRotated.java
@@ -0,0 +1,30 @@
+public class searchRotated{
+    static int searchRotated2(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] == target) {
+                return mid;
+                
+            } else if(arr[left] <= arr[mid]) {
+                if(arr[left] <= target && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(arr[mid] < target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(searchRotated2(new int[] {4,5,6,7,0,1,2}, 0));
+    }
+}
