74a6832103b138fa18df40797fb750115bbd28da
Akash <akashgods222@gmail.com>
2026-09-02 21:57:21 +0530

    Add StringComparison class for string equality checks

---
+++ 07.strings/basics/StringComparison.java
@@ -0,0 +1,8 @@
+public class StringComparison {
+    public static void main(String[] args) {
+        String a = "Akash";
+        String b = "akash";
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+    }
+}
