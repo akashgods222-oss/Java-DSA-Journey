174106a436380e9c0d6cb4e4004da9c4cd0acfb4
Akash <akashgods222@gmail.com>
2026-09-02 21:53:58 +0530

    Add StringBasics class with basic string operations

---
+++ 07.strings/basics/StringBasics.java
@@ -0,0 +1,9 @@
+public class StringBasics {
+    public static void main(String[] args) {
+        String str = " I Love ";
+        String str2 = "Coding";
+        System.out.println(str.length());
+        System.out.println(str.charAt(6));
+        System.out.println(str.concat(str2));
+    }
+}
