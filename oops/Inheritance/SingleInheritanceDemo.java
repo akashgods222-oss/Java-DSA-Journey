fa1a8f3d8ea5e3f848b4194101b345fdb68b715c
Akash <akashgods222@gmail.com>
2026-09-24 20:54:55 +0530

    Add single inheritance demo example

---
+++ oops/Inheritance/SingleInheritanceDemo.java
@@ -0,0 +1,41 @@
+// Parent class (Superclass)
+ class Employee {
+     String name;
+     double salary;
+     
+     //base constructor
+     Employee(String name,double salary) {
+         this.name = name;
+         this.salary = salary;
+     }
+     
+     void displayDetails() {
+         System.out.println("Name : " + name);
+         System.out.println("Salary : " + "Rs " + salary);
+     }
+ }
+
+// Child class(Subclass) accquiring properties of Employee 
+class Developer extends Employee {
+    String programmingLanguage;
+    
+    // Constructor using super to pass value to parent constructor
+    Developer (String name, double salary, String programmingLanguage) {
+        super(name,salary); // calls parent constructor
+        this.programmingLanguage = programmingLanguage;
+    }
+    
+    void displayDeveloperInfo() {
+        displayDetails(); // calling inherited method
+        System.out.println("Domain : " + programmingLanguage + " Developer");
+    }
+}
+
+public class SingleInheritanceDemo {
+    public static void main(String[] args) {
+        // Creating child class object
+        Developer dev = new Developer("Akash",45000,"Java");
+        
+        dev.displayDeveloperInfo();
+    }
+}
